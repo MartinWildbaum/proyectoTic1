e1b22881aa30e953f04224019971265a529ac7f3
GeronimoBasso <gbasso@correo.um.edu.uy>
2021-11-22 13:21:13 -0300

    Hola

--- src/main/java/com/example/primera_version/ui/turist/DenunciaController.java
+++ src/main/java/com/example/primera_version/ui/turist/DenunciaController.java
@@ -1,22 +1,51 @@
 package com.example.primera_version.ui.turist;
 
 import com.example.primera_version.Main;
+import com.example.primera_version.business.CriticaMgr;
+import com.example.primera_version.business.entities.Reserva;
 import com.example.primera_version.ui.Principal;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Component
-public class DenunciaController {
+public class DenunciaController implements Initializable {
+
+    @FXML
+    private Text tituloExperienciaDenuncia;
+
+    @FXML
+    private TextField motivoDeLaDenuncia;
+
+    @FXML
+    private TextField descrpicionDenuncia;
+
+    @FXML
+    private CriticaMgr criticaMgr;
+
+    @FXML
+    private Button bottonConfirmar;
+
+    @Autowired
+    private MisReservasController misReservasController;
 
     @FXML
     void volverAlPerfil(ActionEvent actionEvent) throws Exception{
@@ -72,4 +101,49 @@ public class DenunciaController {
         });
     }
 
+    @FXML
+    void confirmarDenuncia(ActionEvent event){
+        if (motivoDeLaDenuncia.getText().isBlank() || descrpicionDenuncia.getText().isBlank()) {
+            showAlert(
+                    "Error en los datos!",
+                    "Por favor ingrese los datos correctamente.");
+
+        }
+
+
+        String motivo = motivoDeLaDenuncia.getText();
+        String descrpicio=descrpicionDenuncia.getText();
+
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        DropShadow shadow = new DropShadow();
+        tituloExperienciaDenuncia.setText(misReservasController.getMisReservas().getSelectionModel().getSelectedItem().getExperiencia().getTituloExperiencia());
+        animarBoton(bottonConfirmar, shadow);
+
+
+    }
+
+    private void showAlert(String title, String contextText) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(contextText);
+        alert.showAndWait();
+    }
+
+    @FXML
+    void close(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setControllerFactory(Main.getContext()::getBean);
+        AnchorPane root = fxmlLoader.load(Reserva.class.getResourceAsStream("ReservaTurist.fxml"));
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
